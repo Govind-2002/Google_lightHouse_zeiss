156388da897d55ca380092f56d84f630f1c8ebfe
Mali <govind-ram.mali@zeiss.com>
2026-02-24 09:50:46 +0530

    Add NIC resource for VM

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,3 +64,53 @@ resource "azurerm_network_security_rule" "allow_4008" {
 
 }
 
+# create network interface
+
+resource "azurerm_network_interface" "nic" {
+  name                = "lighthouse-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Create Virtual Machine
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "lighthouse-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  depends_on = [azurerm_network_interface_security_group_association.nic_nsg]
+
+  admin_password = "YourPassword123!"
+
+  disable_password_authentication = false
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+}
